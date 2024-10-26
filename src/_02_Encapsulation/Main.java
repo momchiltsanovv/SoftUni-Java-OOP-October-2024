a280c7c88930699443c81b24cb7fa27c48b38d78
Momchil Tsanov <momchil395@gmail.com>
2024-10-26 10:33:42 +0300

    test

---
+++ src/_02_Encapsulation/Main.java
@@ -0,0 +1,12 @@
+package _02_Encapsulation;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
